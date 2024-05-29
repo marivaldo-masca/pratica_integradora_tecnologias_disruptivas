b0e1062365056fc36f292f54dcf188153faf7d3b
marivaldo-masca <marivaldo.m.jr@gmail.com>
2024-05-28 21:48:53 -0300

    Adicionando menu na AppBar

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
  
 function Header() {
     return (
@@ -19,9 +19,9 @@ function Header() {
               aria-label="menu"
               sx={{ mr: 2 }}
             >
-              {/*  
+              {  
               <MenuIcon />
-              */}
+              }
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Gerenciamento de Projetos
